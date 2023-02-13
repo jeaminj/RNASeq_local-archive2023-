9a5948cef978dbc8ea7bf263f9bade18a710176c
Jeamin Jung <79716466+jeaminj@users.noreply.github.com>
2023-02-13 17:27:33 -0500

    Update starGenerateIndices.sh

--- starGenerateIndices.sh
+++ starGenerateIndices.sh
@@ -3,7 +3,7 @@
 #Assumes script is run in environment containing neccessary packages
 
 #change working directory
-cd /Users/jeamin/Documents/bioinfo/bc_tissue_rnaSeq/scripts
+cd /Users/jeamin/Documents/bioinfo/bc_tissue_rnaSeq/
 
 echo "Generating RNASTAR genome files"
 #Create genome index for STAR to utilize
@@ -11,7 +11,7 @@ STAR \
 --runMode genomeGenerate \
 --genomeDir index/starRefs/ \
 --genomeFastaFiles index/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa \
---sjdbGTFFile index/Homo_sapiens.GRCh38.108.gtf \
+--sjdbGTFfile index/Homo_sapiens.GRCh38.108.gtf \
 --runThreadN 4
 
 echo "Index generation complete!"
