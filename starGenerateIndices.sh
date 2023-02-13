122957a53af08db2233a28c385828e7631c2bdbe
Jeamin Jung <79716466+jeaminj@users.noreply.github.com>
2023-02-13 17:11:37 -0500

    Add files via upload

---
+++ starGenerateIndices.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+#Assumes script is run in environment containing neccessary packages
+
+#change working directory
+cd /Users/jeamin/Documents/bioinfo/bc_tissue_rnaSeq/scripts
+
+echo "Generating RNASTAR genome files"
+#Create genome index for STAR to utilize
+STAR \
+--runMode genomeGenerate \
+--genomeDir index/starRefs/ \
+--genomeFastaFiles index/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa \
+--sjdbGTFFile index/Homo_sapiens.GRCh38.108.gtf \
+--runThreadN 4
+
+echo "Index generation complete!"
