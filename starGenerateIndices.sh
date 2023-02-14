415d4e15adaee849dba7c77da7324d82048292d2
Jeamin Jung <jeaminjames@gmail.com>
2023-02-14 14:09:37 -0500

    comment on disk space needed, directory name change

--- starGenerateIndices.sh
+++ starGenerateIndices.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 #Assumes script is run in environment containing neccessary packages
+#100GB of free disk space is recommended before running
 
 #change working directory
 cd /Users/jeamin/Documents/bioinfo/bc_tissue_rnaSeq/
@@ -9,9 +10,9 @@ echo "Generating RNASTAR genome files"
 #Create genome index for STAR to utilize
 STAR \
 --runMode genomeGenerate \
---genomeDir index/starRefs/ \
+--genomeDir index/starGenome/ \
 --genomeFastaFiles index/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa \
 --sjdbGTFfile index/Homo_sapiens.GRCh38.108.gtf \
 --runThreadN 4
 
-echo "Index generation complete!"
+echo "Done!"
