285b8d5a757c987598432d05061937b116a3823f
Jeamin Jung <jeaminjames@gmail.com>
2023-03-02 13:31:14 -0500

    updated to match rnaSeqPipeline.sh

--- HISAT2align.sh
+++ HISAT2align.sh
@@ -3,25 +3,21 @@
 #set working directory
 cd /Users/jeamin/Documents/bioinfo/bc_tissue_rnaSeq/
 
-fastq_reads=/Users/jeamin/Documents/bioinfo/bc_tissue_rnaSeq/trimmedData
+srrArray=("SRR15852396" "SRR15852426")
 
-for base in SRR15852396_tumor SRR15852426_normal
+for srr in ${srrArray[@]};
 do
-  echo $base
+  #Set value for forward paired and reverse paired files
+  fq_1p=${srr}_trimmed_1P.fastq
+  fq_2p=${srr}_trimmed_2P.fastq
 
-  #defines R1 and R2 fastq filename
-  fq1=$fastq_reads/${base}_1P.fastq
-  echo "fq1 =" $fq1
-
-  fq2=$fastq_reads/${base}_2P.fastq
-  echo "fq2 =" $fq2
-
+  echo begginning alignment of $srr to human genome
   hisat2 \
   -q --rna-strandness R \
   -x HISAT2/hisatGenomes/grch38/genome \
-  -1 $fq1 \
-  -2 $fq2 \
-  | samtools sort -o mappedReads/${base}.bam
+  -1 trimmedData/$fq_1p \
+  -2 trimmedData/$fq_2p \
+  | samtools sort -o mappedReads/${srr}.bam
 
 done
 
