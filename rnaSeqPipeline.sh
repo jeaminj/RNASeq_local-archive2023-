8110c76f739a278d1c8fa43d88d13c90d092a63d
Jeamin Jung <79716466+jeaminj@users.noreply.github.com>
2023-04-02 16:43:25 -0400

    Update rnaSeqPipeline.sh

--- rnaSeqPipeline.sh
+++ rnaSeqPipeline.sh
@@ -2,10 +2,10 @@
 # This script is a locally optimized demonstration of an RNASeq workflow, and thus does not contain the syntax or parameters 
 # to be executed on cloud or an HPC cluster
 #
+# This script assumes all necessary packages are correctly installed in an (Anaconda) environment and will be run in such
+# 
 # This script takes paired-end sequence data in FastQ format and then generates a corresponding mapped.bam file -
 # and a processed count matrix file ready for differential gene expression analysis
-# 
-# This script assumes all necessary packages are correctly installed in an Anaconda environment and will be run in such
 #
 # A docker image containing all required packages to run this script can be pulled from jeaminj/data_process_tools 
 # 
@@ -14,12 +14,12 @@
 SECONDS=0
 
 # Array containing srr accession numbers 
-srrArray=("SRR15852426" "SRR15852396")
+srrArray=("SRR15852396" "SRR15852426" "SRR15852429" "SRR15852399" "SRR15852443" "SRR15852413")
 
 # directories
 sraDir=/home/sra/
 wd=/home
-adapter_dir=/opt/conda/envs/bioenv/share/trimmomatic/adapters
+rawData=/home/rawData
 
 # Step [0]: Retrieve SRA Data (To be commented out if data already in possession)
 # ----------------------------------------------------------------------------------------------------
@@ -34,9 +34,6 @@ do
   echo "begginning fasterq-dump, fastq files will be stored in rawData/"
   fasterq-dump $srr -O ../rawData/
 done
-
-# (Optional) Delete files created from prefetch to free up disk space 
-rm -r SRR*
 
 # Step [1]: Quality Control with fastqc
 # ----------------------------------------------------------------------------------------------------
@@ -57,24 +54,25 @@ do
 
     echo "FastQC done, reports stored in rawData/"
 
-# Step [2]: Trimming with trimmomatic
+# Step [2]: Trimming adapters and filtering with fastp
 # ----------------------------------------------------------------------------------------------------
 #change adapter file accordingly
+cd $wd
+echo "Starting fastp for" ${srr}
 
-    trimmomatic PE \
-    -threads 4 \
-    -phred33 \
-    -trimlog trimmedData/${srr}_trimmed.log \
-    rawData/${fq_fwd} rawData/${fq_rev} \
-    -baseout trimmedData/${srr}_trimmed.fastq \
-    ILLUMINACLIP:$adapter_dir/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 MINLEN:36    
+    fastp \
+    -i rawData/$fq_fwd \
+    -I rawData/$fq_rev \
+    --out1 trimmedData/${srr}_trimmed_1P.fastq \
+    --out2 trimmedData/${srr}_trimmed_2P.fastq \
+    --length_required 125
 
-    echo "Trimming completed for" $srr ", outputs stored in /trimmedData"
+    echo "Trimming completed for" $srr ", outputs stored in "trimmedData" directory"
 done
 
 # Step [2.5]: quality checking the now trimmed data (ignoring unpaired reads)
 # ----------------------------------------------------------------------------------------------------
-fastqc trimmedData/*P.fastq -o trimmedData/
+fastqc trimmedData/*.fastq -o trimmedData/
 echo "QC on paired trimmed data complete, begginning alignment!"
 
 # Step [3]: Aligning with HISAT2
@@ -90,7 +88,7 @@ do
 
   echo begginning alignment of $srr to human genome
   hisat2 \
-  -q --rna-strandness R \
+  -q --rna-strandness RF \
   -x HISAT2/hisatGenomes/grch38/genome \
   -1 trimmedData/$fq_1p \
   -2 trimmedData/$fq_2p \
@@ -103,17 +101,21 @@ done
 # download gtf file (variable to updates)
 # wget https://ftp.ensembl.org/pub/release-108/gtf/homo_sapiens/Homo_sapiens.GRCh38.108.gtf.gz
 
-# we will input both our bams in one command and get one output file
+# we will input all our bams in one command and get one output file
 featureCounts \
--p -s 2 -a index/Homo_sapiens.GRCh38.109.gtf \
--o quants/bc_tissue_featureCounts.txt mappedReads/SRR15852396.bam mappedReads/SRR15852426.bam
+-p --countReadPairs -s 2 -a index/Homo_sapiens.GRCh38.109.gtf \
+-o quants/bc_tissue_featureCounts.txt mappedReads/*.bam
 
 # Step [5] Process counts file for DeSEQ2/edgeR 
 # ----------------------------------------------------------------------------------------------------
-# Removes columns and rows that are not needed for the next step and renames column headers to SRR#
-(cat quants/bc_tissue_featureCounts.txt | cut -f1,7,8 | sed '1d' \
-| sed -e "1s/mappedReads\/SRR15852396.bam/SRR15852396/g" \
--e "1s/mappedReads\/SRR15852426.bam/SRR15852426/g") > quants/bc_tissue_counts.txt
+# Keeps only the GeneID and SRR# columns and rows and renames column headers to SRR#
+(cat quants/bc_tissue_featureCounts.txt | cut -f1,7,8,9,10,11,12 | sed '1d' \
+| sed -e "1s/mappedReads\/SRR15852426.bam/SRR15852426/g" \
+-e "1s/mappedReads\/SRR15852396.bam/SRR15852396/g" \
+-e "1s/mappedReads\/SRR15852429.bam/SRR15852429/g" \
+-e "1s/mappedReads\/SRR15852399.bam/SRR15852399/g" \
+-e "1s/mappedReads\/SRR15852443.bam/SRR15852443/g" \
+-e "1s/mappedReads\/SRR15852413.bam/SRR15852413/g") > quants/bc_tissue_counts.txt
 
 # -----------------------------------------------------------------------------------------------------
 
